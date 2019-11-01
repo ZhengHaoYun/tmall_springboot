7d05bf0a652534a63ad1bbad18e10796e51579e9
ZhengHaoYun <1013431730@qq.com>
2019-11-01 10:12:11 +0800

    在application.java处增加了注解，修改了pom文件，使得可以打包此项目

--- src/main/java/com/zhy/Application.java
+++ src/main/java/com/zhy/Application.java
@@ -2,9 +2,19 @@ package com.zhy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
 @SpringBootApplication
-public class Application {
+@ServletComponentScan
+public class Application extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);    
+        SpringApplication.run(Application.class, args);
     }
 }
